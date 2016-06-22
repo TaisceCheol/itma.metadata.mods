910b93cc36fa5ae9ef8783ab5b0d1c4c3bc1ef64
TaisceCheol <piaras.hoban@itma.ie>
2016-06-22 12:38:58 +0100

    added progress bar to make_catalogue...

--- make_catalogue_with_linked_roles.py
+++ make_catalogue_with_linked_roles.py
@@ -1,4 +1,4 @@
-import json
+import json,click
 from lxml import etree
 
 catalogue = etree.parse('itma.cat.soutron_20160216.xml')
@@ -6,19 +6,21 @@ linked_roles = etree.parse('itma.roles.linked.xml')
 
 linked_cat = etree.Element('recordlist')
 
-for record in catalogue.xpath('/recordlist/record'):
-	refno = record.xpath('ITMAReference/text()')[0]
-	people = record.xpath('People')
-	for name in people:
-		match = linked_roles.xpath('/NamedRoles/NamedRole[@id="%s"][Name/text()="%s"]' % (refno,name.text))
-		if len(match):
-			name.attrib['name'] = name.text	
-			name.text = None
-			for item in match:
-				roles = item.xpath('Role')
-				for role in roles:
-					name.append(role)
-	linked_cat.append(record)
-	break
+with click.progressbar(catalogue.xpath('/recordlist/record'),label='Updating soutron xml') as bar:
+	for record in bar:
+	#for record in catalogue.xpath('/recordlist/record'):
+		refno = record.xpath('ITMAReference/text()')[0]
+		people = record.xpath('People')
+		for name in people:
+			match = linked_roles.xpath('/NamedRoles/NamedRole[@id="%s"][Name/text()="%s"]' % (refno,name.text))
+			if len(match):
+				name.attrib['name'] = name.text	
+				name.text = None
+				for item in match:
+					roles = item.xpath('Role')
+					for role in roles:
+						name.append(role)
+		linked_cat.append(record)
+	#break
 
 etree.ElementTree(linked_cat).write('itma.linked.cat.xml',pretty_print=True)
