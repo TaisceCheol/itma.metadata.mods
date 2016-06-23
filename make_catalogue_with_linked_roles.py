6b5741598e856ef28013a005d50601b823394442
TaisceCheol <piaras.hoban@itma.ie>
2016-06-23 12:17:01 +0100

    refactoring make_catalogue_with_linked_roles.py to use map and simple threaded execution; lxml and default multiprocessing.pool.map don't play nice together...why?

--- make_catalogue_with_linked_roles.py
+++ make_catalogue_with_linked_roles.py
@@ -1,26 +1,54 @@
 import json,click
 from lxml import etree
+from itertools import repeat
+from multiprocessing.dummy import Pool
+import time
 
+start_time = time.time()
+
+def process_match(item,name):
+	roles = item.xpath('Role')
+	for role in roles:
+		name.append(role)
+
+def process_name(name,refno):
+	global pool
+	#print name,refno
+	match = linked_roles.xpath('/NamedRoles/NamedRole[@id="%s"][Name/text()="%s"]' % (refno,name.text))
+	if len(match):
+		name.attrib['name'] = name.text	
+		name.text = None
+		map(process_match,match,repeat(name,len(match)))
+
+def process_record(record):
+	global linked_cat
+	record_id = record.xpath('ITMAReference/text()')
+	if len(record_id) == 0:
+		refno = record.attrib['CID']
+	else:
+		refno = record_id[0]
+	people = record.xpath('People')
+	if len(people):
+		map(process_name,people,repeat(refno,len(people)))
+	linked_cat.append(record)
+	return None
+
+# import source records
+print 'Reading catalogue records'
 catalogue = etree.parse('itma.cat.soutron_20160216.xml')
 linked_roles = etree.parse('itma.roles.linked.xml')
-
+print 'Catalogue records read'
+# root node for linked records
 linked_cat = etree.Element('recordlist')
 
-with click.progressbar(catalogue.xpath('/recordlist/record'),label='Updating soutron xml') as bar:
-	for record in bar:
-	#for record in catalogue.xpath('/recordlist/record'):
-		refno = record.xpath('ITMAReference/text()')[0]
-		people = record.xpath('People')
-		for name in people:
-			match = linked_roles.xpath('/NamedRoles/NamedRole[@id="%s"][Name/text()="%s"]' % (refno,name.text))
-			if len(match):
-				name.attrib['name'] = name.text	
-				name.text = None
-				for item in match:
-					roles = item.xpath('Role')
-					for role in roles:
-						name.append(role)
-		linked_cat.append(record)
-	#break
+# multiprocessing pool
+pool = Pool(processes=4)
+# list of source records
+records = catalogue.xpath('/recordlist/record')
+# process records
+pool.map(process_record,records)
 
+# write linked catalogue xml
 etree.ElementTree(linked_cat).write('itma.linked.cat.xml',pretty_print=True)
+
+print "--- %s ---" % (time.time() - start_time)
