1a366d0cfd7c4be64f34a0714784dea0cf4d0c5c
TaisceCheol <piaras.hoban@itma.ie>
2016-06-24 13:37:33 +0100

    refactored using Classes and removing global variables

--- generate_linked_roles.py
+++ generate_linked_roles.py
@@ -1,2 +1,17 @@
+# from linkr import role_extractor,resolve2auths,link_roles
+from linkr import link2catalog,resolve2auths,role_extractor,map_roles
 
+soutron_cat = 'itma.cat.soutron_20160216.xml'
+role_path = 'itma.roles.xml'
+roles_lookup_path = 'linked_roles_lookup.json'
+linked_role_path = 'itma.roles.linked.xml'
+linked_cat_path = 'itma.cat.linked.xml'
+
+#Pipeline Extract -> Resolve -> Map -> Link
+
+role_extractor.Extract(soutron_cat,role_path)
+r = resolve2auths.Resolvr()
+r.link_roles(role_path,roles_lookup_path)
+map_roles.MapR(role_path,roles_lookup_path,linked_role_path)
+link2catalog.Link(soutron_cat,linked_role_path,linked_cat_path)
 
