7f75cacf0420582f7c876964ec8f9023145e16e8
TaisceCheol <piaras.hoban@itma.ie>
2016-06-28 17:26:11 +0100

    reverted to json in linkr/place_extractor.py and improved code for logainm sparql queries and linking

--- linkr/place_extractor.py
+++ linkr/place_extractor.py
@@ -15,7 +15,8 @@ class PlaceR():
 		places = []
 		[[places.append(y.text) for y in x.xpath('*[self::CreationLocation or self::GeographicalLocation]')] for x in records]
 		map(self.locate,places)
-		map(self.link_to_logainm,self.cache.values())
+		cache_values = [json.loads(self.redis_cache.get(k)) for k in self.redis_cache.keys()]
+		map(self.link_to_logainm,cache_values,self.redis_cache.keys())
 		map(self.link_to_record,records)
 
 		etree.ElementTree(self.placelist).write(output_file,pretty_print=True)
@@ -31,18 +32,18 @@ class PlaceR():
 				location = json.loads(self.redis_cache.get(place))
 				el = etree.Element('Location',catid=record.attrib['CID'],type=field.lower())
 				el.text = place
-				for key,value in location.raw.iteritems():
+				for key,value in location.iteritems():
 					el.attrib[key] = unicode(value)
-				if place.split(',')[0] in self.logainm_links.keys():
-					el.attrib['logainm'] = self.logainm_links[place.split(',')[0]]
+				if place in self.logainm_links.keys():
+					el.attrib['logainm'] = self.logainm_links[place]
 				self.placelist.append(el)		
 
 	def link_to_record(self,record):
 		for field in ['GeographicalLocation','CreationLocation']:
 			self.create_xml_nodes(record,field)
-
-	def link_to_logainm(self,place):
-		place = place.address.split(',')[0]
+	
+	def log_sparql_query(self,place,place_key):
+		got_result = False
 		if place not in self.logainm_links.keys():
 			sparql = SPARQLWrapper("http://data.logainm.ie/sparql")
 			sparql.setQuery("""
@@ -59,14 +60,26 @@ class PlaceR():
 			sparql.setReturnFormat(JSON)
 			result = sparql.query().convert()['results']['bindings']
 			for item in result:
-				self.logainm_links[place] = item['place']['value']
+				self.logainm_links[place_key] = item['place']['value']
+				got_result = True
+		return got_result
+
+	def link_to_logainm(self,location,place_key):
+		place = location['address'].split(',')[0]
+		matches = self.log_sparql_query(place,place_key)
+		if matches == True:
+			print self.logainm_links
+		if matches == False:
+			place = location['address'].split(',')[1].strip()
+			self.log_sparql_query(place,place_key)
 
 	def locate(self,place):
 		if place not in self.redis_cache.keys():
 			location = self.geolocator.geocode(place)
 			if location != None:
-				location = json.dumps(location)
-				self.redis_cache.set(place,location)
+				raw_location = location.raw
+				raw_location['address'] = location.address
+				self.redis_cache.set(place,json.dumps(raw_location))
 			else:
 				if len(place.split(',')) > 1:
 					second_order_place = place[place.index(',')+1:].strip()
@@ -74,8 +87,9 @@ class PlaceR():
 						# remove possible event name from place string and try once more to geocode
 						location = self.geolocator.geocode(second_order_place)
 						if location != None:
-							location = json.dumps(location)
-							self.redis_cache.set(place,location)
+							raw_location = location.raw
+							raw_location['address'] = location.address
+							self.redis_cache.set(place,json.dumps(raw_location))
 					else:
 						self.redis_cache.set(place,self.redis_cache.get(second_order_place))
 				else:
